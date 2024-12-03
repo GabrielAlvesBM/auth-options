934bde66318df73d4ab598242d85acfe126468d3
Gabriel Alves <gabrielalves6002@gmail.com>
2024-12-02 21:37:01 -0300

    style: nav and nav animations

--- src/App.vue
+++ src/App.vue
@@ -12,4 +12,27 @@
   <RouterView />
 </template>
 
-<style scoped></style>
+<style scoped>
+.nav {
+  padding: 25px 0 10px 0;
+  display: flex;
+  justify-content: center;
+  gap: 15px;
+}
+
+a {
+  text-decoration: none;
+
+  font-size: 1.1rem;
+
+  color: var(--accent-color);
+
+  transition: transform 0.2s ease-in-out;
+  transition: padding 0.45s ease;
+}
+
+a:hover {
+  padding: 0 15px;
+  transform: scale(1.1);
+}
+</style>
