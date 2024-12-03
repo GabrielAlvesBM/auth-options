b323caf08ea27df52b90365d9aa0614d0efe2d41
Gabriel Alves <gabrielalves6002@gmail.com>
2024-12-02 21:55:41 -0300

    feat: HomeTitle component in HomeView

---
+++ src/components/HomeTitle.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>Auth Options</h1>
+
+  <p>
+    Sistema de autenticação usando VueJS, Express e Axios. Com opções de login pelas redes sociais.
+  </p>
+</template>
+
+<style scoped>
+h1 {
+  font-size: 2.5rem;
+  background-color: var(--primary-color);
+}
+
+p {
+  text-align: center;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,20 @@
+<script setup lang="ts">
+import HomeTitle from '../components/HomeTitle.vue'
+</script>
+
 <template>
   <main>
-    Home
+    <HomeTitle />
   </main>
 </template>
+
+<style scoped>
+main {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  margin: 0 20px;
+}
+</style>
