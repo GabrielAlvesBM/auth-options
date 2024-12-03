d4065d4fd3f90ab41330ef915b25a50de778b420
Gabriel Alves <gabrielalves6002@gmail.com>
2024-12-03 14:49:41 -0300

    style(AppNav) optimize responsivity

--- src/components/AppNav.vue
+++ src/components/AppNav.vue
@@ -21,13 +21,29 @@ a {
   font-size: 1.1rem;
 
   color: var(--accent-color);
+  will-change: transform, padding;
 
-  transition: transform 0.2s ease-in-out;
-  transition: padding 0.45s ease;
+  transition:
+    transform 0.2s ease-in-out,
+    padding 0.45s ease;
 }
 
 a:hover {
   padding: 0 15px;
   transform: scale(1.1);
 }
+
+@media (max-width: 540px) {
+  .nav {
+    flex-direction: column;
+    align-content: center;
+    align-items: center;
+    gap: 15px;
+  }
+
+  a:hover {
+    padding: 15px 0;
+  }
+}
+
 </style>
