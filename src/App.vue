a4a302510c3e072e62096addc2563446ea461568
Gabriel Alves <gabrielalves6002@gmail.com>
2024-12-02 21:41:00 -0300

    refactor: reorganize code and implement AppNav component

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import AppNav from './components/AppNav.vue'
+</script>
 
 <template>
   <header>
-    <nav class="nav">
-      <RouterLink to="/">Página Inicial</RouterLink>
-      <RouterLink to="/login">Entrar</RouterLink>
-      <RouterLink to="/register">Registrar</RouterLink>
-    </nav>
+    <AppNav />
   </header>
 
   <RouterView />
 </template>
-
-<style scoped>
-.nav {
-  padding: 25px 0 10px 0;
-  display: flex;
-  justify-content: center;
-  gap: 15px;
-}
-
-a {
-  text-decoration: none;
-
-  font-size: 1.1rem;
-
-  color: var(--accent-color);
-
-  transition: transform 0.2s ease-in-out;
-  transition: padding 0.45s ease;
-}
-
-a:hover {
-  padding: 0 15px;
-  transform: scale(1.1);
-}
-</style>
---
+++ src/components/AppNav.vue
@@ -0,0 +1,32 @@
+<template>
+  <nav class="nav">
+    <RouterLink to="/">Página Inicial</RouterLink>
+    <RouterLink to="/login">Entrar</RouterLink>
+    <RouterLink to="/register">Registrar</RouterLink>
+  </nav>
+</template>
+
+<style scoped>
+.nav {
+  padding: 25px 0 10px 0;
+  display: flex;
+  justify-content: center;
+  gap: 15px;
+}
+
+a {
+  text-decoration: none;
+
+  font-size: 1.1rem;
+
+  color: var(--accent-color);
+
+  transition: transform 0.2s ease-in-out;
+  transition: padding 0.45s ease;
+}
+
+a:hover {
+  padding: 0 15px;
+  transform: scale(1.1);
+}
+</style>
