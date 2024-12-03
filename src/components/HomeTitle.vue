5eba51f2c3748cc3582ae87d125be287a9856b58
Gabriel Alves <gabrielalves6002@gmail.com>
2024-12-02 22:23:17 -0300

    style: restyle HomeTitle component

--- src/components/HomeTitle.vue
+++ src/components/HomeTitle.vue
@@ -4,15 +4,29 @@
   <p>
     Sistema de autenticação usando VueJS, Express e Axios. Com opções de login pelas redes sociais.
   </p>
+
+  <p class="pitch">
+    <RouterLink to="/login">Entre</RouterLink> ou
+    <RouterLink to="/register">Se Registre</RouterLink>
+  </p>
 </template>
 
 <style scoped>
 h1 {
-  font-size: 2.5rem;
+  font-size: 3rem;
   background-color: var(--primary-color);
 }
 
 p {
   text-align: center;
+}
+
+.pitch {
+  margin-top: 150px;
+  font-size: 1.4rem;
+}
+
+.pitch a {
+  font-size: 1.6rem;
 }
 </style>
