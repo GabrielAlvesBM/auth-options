17eaaae80408b528752a082453f906de1f6a8d5e
Gabriel Alves <gabrielalves6002@gmail.com>
2024-12-02 21:13:37 -0300

    feat: LoginForm and /login page

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,50 @@
+<template>
+  <form action="#">
+    <input type="email" name="email" id="email" placeholder="E-mail" autocomplete="off" required />
+    <input
+      type="password"
+      name="password"
+      id="password"
+      placeholder="Senha"
+      autocomplete="off"
+      required
+      minlength="4"
+      maxlength="255"
+    />
+
+    <button type="submit">Enviar</button>
+  </form>
+</template>
+
+<style scoped>
+form {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+}
+
+input {
+  width: 400px;
+  padding: 12.5px;
+
+  border-radius: 7.5px;
+  border: none;
+}
+
+button {
+  padding: 12.5px;
+  margin-top: 1em;
+
+  border-radius: 90000px;
+  border: none;
+
+  color: var(--color-text);
+  background-color: var(--primary-color);
+
+  transition: background-color 0.3s ease;
+}
+
+button:hover {
+  background-color: var(--secondary-color);
+}
+</style>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,5 +1,18 @@
+<script setup lang="ts">
+import LoginForm from '../components/LoginForm.vue'
+</script>
+
 <template>
   <main>
-    Login
+    <LoginForm />
   </main>
 </template>
+
+<style scoped>
+main {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: calc(100dvh - 100px);
+}
+</style>
